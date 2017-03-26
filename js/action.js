8a709c768b3e4a87a9c129c981fb14982554defe
adminet <guy.diamond.x@gmail.com>
2017-03-26 23:29:35 +0100

    MVC skeleton

---
+++ js/action.js
@@ -0,0 +1,36 @@
+var view = {
+  introductionCreature: function(){
+  },
+  displayStartButton: function() {
+  },
+  runHidding: function(){
+  },
+  showUp: function() {
+  },
+  miss: function(){
+  },
+  hit: function(){
+  },
+  congratulations: function(){
+  }
+}
+
+var model = {
+  numCreatures: 4,
+  numLetters: 2,
+  alphabet: ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"],
+  coordinatePlacesHidden: [{locations: [30,5]}, {locations: [60,5]}, {locations: [25,20]}, {locations: [75,20]}, {locations: [20,25]}, {locations: [80,25]}],
+  chooseCreatures: function(){
+  },
+  chooseLetters: function(){
+  },
+  choosePlacesHidden: function(){
+  },
+  checkPair: function(){
+  }
+}
+var contnroller ={
+    numClick: 0,
+    parseClick: function(){
+    }
+}
