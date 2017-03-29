ede9a405ceef3f91bbdd76687212830a610a5433
Guy Diamond <guy.diamond.x@gmail.com>
2017-03-30 00:28:20 +0100

    Random pictures selection

--- js/action.js
+++ js/action.js
@@ -1,5 +1,12 @@
 var view = {
-  introductionCreature: function(){
+introductionCreature: function(numbPic){
+  console.log(numbPic);
+    var imgs = document.querySelectorAll('.character');
+    for (let i=0; i<numbPic.length; i++){
+    var num=numbPic[i];
+   imgs[i].src='img/postac' + num + '.jpg';
+    console.log(num);
+    }
   },
   displayStartButton: function() {
   },
@@ -14,13 +21,22 @@ var view = {
   congratulations: function(){
   }
 }
-
 var model = {
   numCreatures: 4,
+  numPictures: 8,
   numLetters: 2,
   alphabet: ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"],
   coordinatePlacesHidden: [{locations: [30,5]}, {locations: [60,5]}, {locations: [25,20]}, {locations: [75,20]}, {locations: [20,25]}, {locations: [80,25]}],
+
   chooseCreatures: function(){
+      var arrNumb = []
+      while(arrNumb.length < this.numCreatures){
+      var randomnumber = Math.ceil(Math.random()*this.numPictures)
+      if(arrNumb.indexOf(randomnumber) > -1) continue;
+      arrNumb[arrNumb.length] = randomnumber;
+      }
+      console.log(typeof arrNumb);
+      view.introductionCreature(arrNumb);
   },
   chooseLetters: function(){
   },
@@ -34,3 +50,4 @@ var contnroller ={
     parseClick: function(){
     }
 }
+ model.chooseCreatures();
