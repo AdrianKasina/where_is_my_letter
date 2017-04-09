f2d614848619e586ed3fb8fdc8879f36cf6c505c
Guy Diamond <guy.diamond.x@gmail.com>
2017-04-09 20:50:55 +0100

    added moving with GSAP

--- js/action.js
+++ js/action.js
@@ -1,71 +1,119 @@
+window.onload
 var view = {
-introductionCreature: function(numbPic){
-    var imgs = document.querySelectorAll('.character');
-    for (var i=0; i<numbPic.length; i++){
-    var num=numbPic[i];
-    imgs[i].src='img/postac' + num + '.jpg';
-    }
-  },
-  myLetters: function(showLetter) {
-    var pairLetters =  document.querySelectorAll('.letter');
-    var j =0;
-      for (var i=0; i<pairLetters.length; i++){
-        if (i%2 == 0) {
-          pairLetters[i].innerHTML = showLetter[j];
-        } else {
-          pairLetters[i].innerHTML = showLetter[j];
-          j++;
-        }
-      }
-  },
-  displayStartButton: function() {
-  },
-  runHidding: function(){
-  },
-  showUp: function() {
-  },
-  miss: function(){
-  },
-  hit: function(){
-  },
-  congratulations: function(){
-  }
+        introductionCreature: function(numbPic) {
+            var imgs = document.querySelectorAll('.character');
+            for (var i = 0; i < numbPic.length; i++) {
+                var num = numbPic[i];
+                imgs[i].src = 'img/postac' + num + '.jpg';
+            }
+        },
+        myLetters: function(showLetter) {
+            var pairLetters = document.querySelectorAll('.letter');
+            var j = 0;
+            for (var i = 0; i < pairLetters.length; i++) {
+                if (i % 2 == 0) {
+                    pairLetters[i].innerHTML = showLetter[j];
+                } else {
+                    pairLetters[i].innerHTML = showLetter[j];
+                    j++;
+                }
+            }
+        },
+
+        runHidding: function(coordinates) {
+            var characters = document.querySelectorAll('.picture');
+            for (i = 0; i < characters.length; i++) {
+              var heightCharacters = characters[i].clientHeight,
+                  widthCharacter = characters[i].clientWidth;
+            TweenMax.to(characters[i], 2, {left:(coordinates[i][0]-(widthCharacter/2)), top:(coordinates[i][1])-heightCharacters, scale:0.9, opacity:0.9, ease: Back.easeOut.config(1)});
+            }
+
+    },
+    showUp: function() {},
+    miss: function() {},
+    hit: function() {},
+    congratulations: function() {}
 }
 var model = {
-  numCreatures: 4,
-  numPictures: 8,
+    numCreatures: 4,
+    numPictures: 8,
+    locations: 6,
+    alphabet: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
 
-  alphabet: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-  coordinatePlacesHidden: [{locations: [30,5]}, {locations: [60,5]}, {locations: [25,20]}, {locations: [75,20]}, {locations: [20,25]}, {locations: [80,25]}],
+    chooseCreatures: function() {
+        var arrNumb = [];
+        while (arrNumb.length < this.numCreatures) {
+            var randomNumber = Math.ceil(Math.random() * this.numPictures)
+            if (arrNumb.indexOf(randomNumber) > -1) continue;
+            arrNumb[arrNumb.length] = randomNumber;
+        }
+        view.introductionCreature(arrNumb);
+    },
 
-  chooseCreatures: function(){
-      var arrNumb = []
-      while(arrNumb.length < this.numCreatures){
-      var randomNumber = Math.ceil(Math.random()*this.numPictures)
-      if(arrNumb.indexOf(randomNumber) > -1) continue;
-      arrNumb[arrNumb.length] = randomNumber;
+    chooseLetters: function() {
+        var arrLetters = [];
+        while (arrLetters.length < this.numCreatures / 2) {
+            var randomLetter = Math.floor(Math.random() * 26)
+            if (arrLetters.indexOf(randomLetter) > -1) continue;
+            var letter = this.alphabet[randomLetter];
+            arrLetters[arrLetters.length] = letter;
+        }
+        view.myLetters(arrLetters);
+    },
+
+
+    coordinatePlacesHidden: function () {
+      var getCoordinate = document.querySelectorAll(".places"),
+          getCoordinateBody = document.body.getBoundingClientRect(),
+          arrCoordinates = [];
+      for (var i=0; i<getCoordinate.length; i++) {
+        var coordinates = getCoordinate[i].getBoundingClientRect(),
+            heightPlaces = getCoordinate[i].clientHeight;
+        arrCoordinates[arrCoordinates.length] = [coordinates.left, coordinates.top+heightPlaces];
       }
-      view.introductionCreature(arrNumb);
-  },
-  chooseLetters: function(){
-    var arrLetters =[]
-    while(arrLetters.length < this.numCreatures/2) {
-      var randomLetter = Math.floor(Math.random()*26)
-      if(arrLetters.indexOf(randomLetter) > -1) continue;
-      var letter = this.alphabet[randomLetter];
-      arrLetters[arrLetters.length] = letter;
-    }
-    view.myLetters(arrLetters);
-  },
-  choosePlacesHidden: function(){
-  },
-  checkPair: function(){
-  }
+      model.choosePlacesHidden(arrCoordinates);
+    },
+
+    choosePlacesHidden: function(arrCoordinates) {
+        var arrPlaces = [];
+        var mySet = new Set();
+        var i = 0;
+        while (arrPlaces.length < this.numCreatures) {
+            var randomNumber = Math.floor(Math.random() * this.locations);
+            var randomPlaces = arrCoordinates[randomNumber];
+            mySet.add(randomPlaces);
+            var arrPlaces = Array.from(mySet);
+        }
+        view.runHidding(arrPlaces);
+    },
+
+
+    checkPair: function() {}
 }
-var contnroller ={
+
+var contnroller = {
     numClick: 0,
-    parseClick: function(){
+    displayStartButton: function() {
+      var button = document.getElementById("start");
+        button.addEventListener("click", function() {
+            button.style.display = "none";
+            model.coordinatePlacesHidden();
+            contnroller.parseClick();
+        })
+    },
+    parseClick: function() {
+      var characters = document.querySelectorAll('.picture');
+      for (var i=0; i<characters.lenght; i++)  {
+        characters[i].addEventListener("click", function() {
+        TweenMax.to(characters[i], 1, {left:-5+"px", ease: Back.easeOut.config(1)});
+        console.log("kliknales");
+        })
+      }
     }
 }
- model.chooseCreatures();
- model.chooseLetters();
+
+window.addEventListener("load", function(event) {
+  model.chooseCreatures();
+  model.chooseLetters();
+  contnroller.displayStartButton();
+});
